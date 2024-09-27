d460223c4183681f584157d3faad2a04ecb721d4
Doni Nurramdan <doni.nurramdan@indocyber.id>
2024-09-27 14:41:07 +0700

    add mitigating bad request

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -3,16 +3,20 @@ const { User } = require("../models");
 
 exports.cekUser = async (req, res) => {
   const { body } = req;
-  console.log(body);
-  const cekUser = await User.findOne({
-    where: {
-      userId: body.userId,
-      password: body.password,
-    },
-  });
-  if (cekUser != null) {
-    res.status(200).json({ message: "Login successfully.", data: cekUser });
+  //   console.log(body);
+  if (body.userId && body.password) {
+    const cekUser = await User.findOne({
+      where: {
+        userId: body.userId,
+        password: body.password,
+      },
+    });
+    if (cekUser != null) {
+      res.status(200).json({ message: "Login successfully.", data: cekUser });
+    } else {
+      res.status(200).json({ message: "Problem to Login" });
+    }
   } else {
-    res.status(200).json({ message: "Problem to Login" });
+    res.status(400).json({ message: "Bad Request" });
   }
 };
